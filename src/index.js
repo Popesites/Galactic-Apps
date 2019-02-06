b64a20230fa9209ef0fc0d0dd869db94b228f954
Popesites <conner@popesites.tech>
2019-02-06 13:39:08 -0800

    Removed unnecessary files

--- src/index.js
+++ src/index.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import HomeFive from './HomeFive';
-import Login from './Login';
-import SignUp from './SignUp';
-import PasswordForget from './PasswordForget';
 import registerServiceWorker from './registerServiceWorker';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Terms from "./Terms";
@@ -15,9 +12,6 @@ class Root extends React.Component {
   		<BrowserRouter basename={'/'} >
 		  	<Switch>
 				<Route exact path={`${process.env.PUBLIC_URL}/`} component={HomeFive}/>
-				<Route path={`${process.env.PUBLIC_URL}/login`} component={Login}/>
-			  	<Route path={`${process.env.PUBLIC_URL}/password-forget`} component={PasswordForget}/>
-			  	<Route path={`${process.env.PUBLIC_URL}/sign-up`} component={SignUp}/>
                 <Route path={`${process.env.PUBLIC_URL}/terms-of-service`} component={Terms}/>
                 <Route path={`${process.env.PUBLIC_URL}/privacy-policy`} component={Privacy}/>
 			</Switch>
